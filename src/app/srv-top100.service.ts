47a872da7d47d307c5ad023c1ec1c3d32c85f491
Prateek Saxena <prateeksaxena@ufl.edu>
2018-04-14 22:27:10 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/app/srv-top100.service.ts
+++ src/app/srv-top100.service.ts
@@ -2,17 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Top100 } from './models/top100.model';
 import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class SrvTop100Service {
-  private serviceUrl="http://2e8a03bf.ngrok.io/test";
+  private serviceUrl="http://2759c4d1.ngrok.io/test";
 
   constructor(private http: HttpClient) { }
 
   getData(){
     const body = {sql: "select * from (select * from crime order by date_occured desc) where rownum<=25"};
-    return this.http.post<Top100[]>(this.serviceUrl,body);
   }
-}
+    return this.http.post<Top100[]>(this.serviceUrl,body);
